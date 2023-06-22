89bf293d84a0f86673cc695214883bae1d6b7cde
Rizqi Satya Haprabu <112357810+haprabb@users.noreply.github.com>
2023-06-22 21:36:58 +0700

    iki to coba2 tok cc

--- src/Main/javaClasscooba.java
+++ src/Main/javaClasscooba.java
@@ -2,7 +2,7 @@ package Main;
 
 public class javaClasscooba {
     public static void main(String[]args){
-        System.out.println("hellooww word");
+        System.out.println("helllooww word");
     }
 
 }
