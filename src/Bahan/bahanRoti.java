6dd4e4de5622f27c0535a40d31519066c6f54133
Rizqi Satya Haprabu <112357810+haprabb@users.noreply.github.com>
2023-06-23 19:33:35 +0700

    ini dah tak implements mbe bahan e yess

---
+++ src/Bahan/bahanRoti.java
@@ -0,0 +1,12 @@
+package Bahan;
+
+public interface bahanRoti {
+    abstract int TepungTerigu();
+    abstract int gulaPasir();
+    abstract int butter();
+    abstract int ragi();
+    abstract int susuBubuk();
+    abstract int susuCair();
+    abstract int telur();
+    abstract int esBatu();
+}
---
+++ src/Main/rotiTawar.java
@@ -0,0 +1,36 @@
+package Main;
+import Bahan.bahanRoti;
+
+public class rotiTawar implements bahanRoti {
+    public int TepungTerigu() {
+        return 0;
+    }
+
+    public int gulaPasir() {
+        return 0;
+    }
+
+    public int butter() {
+        return 0;
+    }
+
+    public int ragi() {
+        return 0;
+    }
+
+    public int susuBubuk() {
+        return 0;
+    }
+
+    public int susuCair() {
+        return 0;
+    }
+
+    public int telur() {
+        return 0;
+    }
+
+    public int esBatu() {
+        return 0;
+    }
+}
