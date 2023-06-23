70d48eb0622542ba2cab7eb4899818fc20000fc9
Rizqi Satya Haprabu <112357810+haprabb@users.noreply.github.com>
2023-06-23 20:05:49 +0700

    ini dah tak implements mbe bahan e yess

---
+++ src/MainClass/mainClass.java
@@ -0,0 +1,7 @@
+package MainClass;
+
+public class mainClass {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/TampilanAwal/tampilanAwal.java
@@ -0,0 +1,5 @@
+package TampilanAwal;
+
+public class tampilanAwal {
+    
+}
