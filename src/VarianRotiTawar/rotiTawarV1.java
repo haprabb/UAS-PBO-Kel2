df7804dea2ee6ff72abb1edd11e6193a5d0df059
Rizqi Satya Haprabu <112357810+haprabb@users.noreply.github.com>
2023-06-24 13:34:14 +0700

    terbaru

---
+++ src/VarianRotiTawar/rotiTawarV1.java
@@ -0,0 +1,39 @@
+package VarianRotiTawar;
+import toppingFilling.toppingFilling;
+
+public class rotiTawarV1 implements toppingFilling{
+    @Override
+    public int Keju() {
+        return 0;
+    }
+
+    @Override
+    public int Coklat() {
+        return 0;
+    }
+
+    @Override
+    public int KrimVanilla() {
+        return 0;
+    }
+
+    @Override
+    public int SelaiRedBEan() {
+        return 0;
+    }
+
+    @Override
+    public int Sosis() {
+        return 0;
+    }
+
+    @Override
+    public int SmokeBeef() {
+        return 0;
+    }
+
+    @Override
+    public int BawangBombay() {
+        return 0;
+    }
+}
---
+++ src/VarianRotiTawar/rotiTawarV2.java
@@ -0,0 +1,38 @@
+package VarianRotiTawar;
+import toppingFilling.toppingFilling;
+public class rotiTawarV2 implements toppingFilling{
+    @Override
+    public int Keju() {
+        return 0;
+    }
+
+    @Override
+    public int Coklat() {
+        return 0;
+    }
+
+    @Override
+    public int KrimVanilla() {
+        return 0;
+    }
+
+    @Override
+    public int SelaiRedBEan() {
+        return 0;
+    }
+
+    @Override
+    public int Sosis() {
+        return 0;
+    }
+
+    @Override
+    public int SmokeBeef() {
+        return 0;
+    }
+
+    @Override
+    public int BawangBombay() {
+        return 0;
+    }
+}
