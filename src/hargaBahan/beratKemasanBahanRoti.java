4323fbb27fafc662cccdb300bcab7496bd633485
LENOVO <apriliafatmasari01@student.uns.ac.id>
2023-06-24 21:37:36 +0700

    Bahan roti

--- src/hargaBahan/beratKemasanBahanRoti.java
+++ src/hargaBahan/beratKemasanBahanRoti.java
@@ -1,35 +1,69 @@
 package hargaBahan;
 import Bahan.bahanRoti;
-public class beratKemasanBahanRoti implements bahanRoti {
-    public int TepungTerigu() {
-        return 0;
-    }
 
+import javax.swing.*;
+
+public class beratKemasanBahanRoti implements bahanRoti {
+    int weight;
+    public int TepungTerigu() {
+        if (this.weight > 1000){
+            return this.weight * 40000;
+        } else {
+            return 40000;
+        }
+    }
     public int gulaPasir() {
-        return 0;
+        if (this.weight > 1000){
+            return this.weight * 25000;
+        } else {
+            return 25000;
+        }
     }
 
     public int butter() {
-        return 0;
+        if (this.weight > 500){
+            return this.weight * 23000;
+        } else {
+            return 23000;
+        }
     }
 
     public int ragi() {
-        return 0;
+        if (this.weight > 11){
+            return this.weight * 5000;
+        } else {
+            return 5000;
+        }
     }
-
     public int susuBubuk() {
-        return 0;
+        if (this.weight > 1000){
+            return this.weight * 39000;
+        } else {
+            return 39000;
+        }
     }
 
     public int susuCair() {
-        return 0;
+        if (this.weight > 1000){
+            return this.weight * 24000;
+        } else {
+            return 24000;
+        }
     }
 
     public int telur() {
-        return 0;
+        if (this.weight > 1000){
+            return this.weight * 23000;
+        } else {
+            return 23000;
+        }
+    }
+    public int esBatu() {
+        if (this.weight > 1000){
+            return this.weight * 2000;
+        } else {
+            return 2000;
+        }
     }
 
-    public int esBatu() {
-        return 0;
-    }
 }
--- src/hargaBahan/beratKemasanToppingFilling.java
+++ src/hargaBahan/beratKemasanToppingFilling.java
@@ -1,31 +1,5 @@
 package hargaBahan;
-import toppingFilling.toppingFilling;
-public class beratKemasanToppingFilling implements toppingFilling {
-    public int Keju() {
-        return 0;
-    }
 
-    public int Coklat() {
-        return 0;
-    }
+public class beratKemasanToppingFilling {
 
-    public int KrimVanilla() {
-        return 0;
-    }
-
-    public int SelaiRedBEan() {
-        return 0;
-    }
-
-    public int Sosis() {
-        return 0;
-    }
-
-    public int SmokeBeef() {
-        return 0;
-    }
-
-    public int BawangBombay() {
-        return 0;
-    }
 }
