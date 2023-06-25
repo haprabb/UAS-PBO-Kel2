a6b33975f9ccffe1f01122100b2fbf125cf72fa0
Rizqi Satya Haprabu <112357810+haprabb@users.noreply.github.com>
2023-06-25 17:48:03 +0700

    uwes tak implements sama bikin method weight

--- src/hargaBahan/beratKemasanToppingFilling.java
+++ src/hargaBahan/beratKemasanToppingFilling.java
@@ -1,5 +1,35 @@
 package hargaBahan;
+import toppingFilling.toppingFilling;
 
-public class beratKemasanToppingFilling {
+public class beratKemasanToppingFilling implements toppingFilling {
 
+ int weight;
+
+    public int Keju() {
+        return 0;
+    }
+
+    public int Coklat() {
+        return 0;
+    }
+
+    public int KrimVanilla() {
+        return 0;
+    }
+
+    public int SelaiRedBEan() {
+        return 0;
+    }
+
+    public int Sosis() {
+        return 0;
+    }
+
+    public int SmokeBeef() {
+        return 0;
+    }
+
+    public int BawangBombay() {
+        return 0;
+    }
 }
