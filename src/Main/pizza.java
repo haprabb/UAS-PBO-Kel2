51d19efc07a617912ddacf0e94882c27536398ba
fathiyarima <111724404+fathiyarima@users.noreply.github.com>
2023-06-25 17:41:12 +0700

    pizza

--- src/Main/pizza.java
+++ src/Main/pizza.java
@@ -3,41 +3,49 @@ import Bahan.bahanRoti;
 public class pizza implements bahanRoti{
 
     public int TepungTerigu() {
+
         return 1_000;
     }
 
 
     public int gulaPasir() {
+
         return 100;
     }
 
 
     public int butter() {
+
         return 100;
     }
 
 
     public int ragi() {
+
         return 20;
     }
 
 
     public int susuBubuk() {
+
         return 200;
     }
 
 
     public int susuCair() {
+
         return 180;
     }
 
 
     public int telur() {
+
         return 50;
     }
 
 
     public int esBatu() {
+
         return 300;
     }
 
