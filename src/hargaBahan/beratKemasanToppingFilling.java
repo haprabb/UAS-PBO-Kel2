d19f8b585c044e93aabeff7c6a06151fc67c04d7
Rizqi Satya Haprabu <112357810+haprabb@users.noreply.github.com>
2023-06-26 18:38:05 +0700

    update baru

--- src/hargaBahan/beratKemasanToppingFilling.java
+++ src/hargaBahan/beratKemasanToppingFilling.java
@@ -3,32 +3,39 @@ import toppingFilling.toppingFilling;
 
 public class beratKemasanToppingFilling implements toppingFilling {
 
- int weight;
+int weight;
 
+    @Override
     public int Keju() {
-        return 10;
+        return 0;
     }
 
+    @Override
     public int Coklat() {
         return 0;
     }
 
+    @Override
     public int KrimVanilla() {
         return 0;
     }
 
+    @Override
     public int SelaiRedBEan() {
         return 0;
     }
 
+    @Override
     public int Sosis() {
         return 0;
     }
 
+    @Override
     public int SmokeBeef() {
         return 0;
     }
 
+    @Override
     public int BawangBombay() {
         return 0;
     }
