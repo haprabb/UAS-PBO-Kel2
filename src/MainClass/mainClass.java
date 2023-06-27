6ebfe5bc8903d857e1362b756cd2cb973f7f8eeb
fathiyarima <111724404+fathiyarima@users.noreply.github.com>
2023-06-27 09:50:05 +0700

    hargapesanan

--- src/MainClass/mainClass.java
+++ src/MainClass/mainClass.java
@@ -8,13 +8,34 @@ import hargaBahan.beratKemasanBahanRoti;
 import java.util.Scanner;
 
 public class mainClass {
+
     public static void main(String[] args) {
         tampilanAwal awal = new tampilanAwal();
         awal.MainMenu();
 
-        rotiManis manis = new rotiManis();
+
+        // Membuat objek roti manis
+                rotiManis rotiManis = new rotiManis();
+                bahanRoti bahanRotiManis = rotiManis;
+                beratKemasanBahanRoti beratKemasanRotiManis = new beratKemasanBahanRoti();
+
+                // Menampilkan berat kemasan bahan roti manis
+                System.out.println("Berat Kemasan Bahan Roti Manis:");
+                System.out.println("Tepung Terigu: " + beratKemasanRotiManis.TepungTerigu() + " gram");
+                System.out.println("Gula Pasir: " + beratKemasanRotiManis.gulaPasir() + " gram");
+                System.out.println("Butter: " + beratKemasanRotiManis.butter() + " gram");
+                System.out.println("Ragi: " + beratKemasanRotiManis.ragi() + " gram");
+                System.out.println("Susu Bubuk: " + beratKemasanRotiManis.susuBubuk() + " gram");
+                System.out.println("Susu Cair: " + beratKemasanRotiManis.susuCair() + " gram");
+                System.out.println("Telur: " + beratKemasanRotiManis.telur() + " gram");
+                System.out.println("Es Batu: " + beratKemasanRotiManis.esBatu() + " gram");
+
+                // Menampilkan jumlah adonan roti manis
+                System.out.println("Jumlah Adonan Roti Manis: " + rotiManis.adonanManis() + " pcs");
+            }
+        }
 
 
-        }
-        }
+
+
 
